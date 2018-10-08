a3e6f7223b9b893aac4ccdc3f49a0de1ae401361
anas140 <muhammedanas4140@gmail.com>
2018-10-08 16:17:12 +0530

    add todo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AddTodo from './AddTodo';
 import Todos from './Todos';
 
 class App extends Component {
@@ -17,11 +18,22 @@ class App extends Component {
         todos: todos
       })
   }
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+    
+  }
+
   render() {
     return (
       <div className="container">
         <h1 className="center blue-text">Todos</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
