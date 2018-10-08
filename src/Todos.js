9cfbb93bf5a0f00acb89ca6261f83f3457d3c7d3
anas140 <muhammedanas4140@gmail.com>
2018-10-08 14:52:30 +0530

    styles

--- src/Todos.js
+++ src/Todos.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import './Todos.css'
 
 const Todos = ({todos, deleteTodo}) => {
 	const todoList = todos.length ? (
 			todos.map(todo => {
 				return(
-					<li className="collection-item" key={todo.id} onClick={() => {deleteTodo(todo.id)}}>
-						<span>{todo.content}</span>
+					<li className="collection-item" key={todo.id} >
+						<span>
+						{todo.content}
+						 <i className="material-icons right delete red-text" onClick={() => {deleteTodo(todo.id)}}>
+						 		delete_forever
+				 			</i>
+						</span>
 					</li>
 				);
 			})
